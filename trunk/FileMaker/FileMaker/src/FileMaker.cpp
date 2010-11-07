bc5a7a4fa90bc46a6e3069027d58ca730518362d
luiscarlosch <luiscarlosch@955ba1c3-79b7-fe45-28d5-40bb8a964ae0>
2010-11-07 01:00:17 +0000

    Timer listo

--- trunk/FileMaker/FileMaker/src/FileMaker.cpp
+++ trunk/FileMaker/FileMaker/src/FileMaker.cpp
@@ -11,19 +11,46 @@
 #include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <ctime>
+#include <sys/time.h>
 
 using namespace std;
 
 int main() {
+	struct timespec clock_resolution;
+	int stat;
+
+	//stat=clock_getres(CLOCK_REALTIME, &clock_resolution);
+	//printf("time returns %d seconds \n", time(NULL));
+	//clock_gettime(CLOCK_REALTIME,&clock_resolution);
+
+	//printf("Clock resolution is %d seconds, %ld nanoseconds\n",	clock_resolution.tv_sec, clock_resolution.tv_nsec);
+//******************************************************************************************************
+	timeval t1, t2;
+	long double elapsedTime;
+	gettimeofday(&t1,NULL);
+
+
+
+//******************************************************************************************************
 
 	ofstream myfile;
 	timespec ts;
 	myfile.open("10MB.txt");
-	for(int i=0; i<1000000000; i++){
+	for(int i=0; i<10000000; i++){
 		myfile<<"*";
 	}
 
 	myfile.close();
-	cout<<"d "<<&ts;
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+//	cout<<"d "<<&ts;
+	cout << "\n!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	gettimeofday(&t2,NULL);
+	elapsedTime = (t2.tv_usec-t1.tv_usec);
+
+	elapsedTime = ((t2.tv_sec-t1.tv_sec)*1000.0)+((t2.tv_usec-t1.tv_usec)/1000.0);
+
+	cout<<elapsedTime<<" ms.";
+	//printf("%ld\n",(t2.tv_sec-t1.tv_sec));
 }
